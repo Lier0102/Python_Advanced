e0da92a94dc2f471ef331159eb20ad10fa1bdb57
canbesad <75836412+Lier0102@users.noreply.github.com>
2022-09-17 09:18:02 +0900

    committed

--- C4N_y0u_S0lv3_i7/solve.py
+++ C4N_y0u_S0lv3_i7/solve.py
@@ -1,12 +1,12 @@
 class Calculator:
-    def __init__(self):
+    def __init__(self): # 생성자
         self.value = 0
 
-    def add(self, val):
-        self.value += val
+    def add(self, val): # 덧셈 함수
+        self.value += val # 현재 value에 val만큼 더함
 
-class UpgradeCalculator(Calculator):
-    def minus(self, val):
+class UpgradeCalculator(Calculator): # 추가적인 기능을 더한 계산기
+    def minus(self, val): # 상속을 받으며, minus 함수까지 추가.
         self.value -= val
 
 cal = UpgradeCalculator()
